6673ee3cf9086d9143f1a5632175cc4da0725300
Marufibragimov11 <ibragimovmaruf1108@gmail.com>
2024-10-28 13:12:57 -0400

    new commit

--- goodreads/urls.py
+++ goodreads/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('books.urls'), name='books'),
+    path('admin/', admin.site.urls),
     path('users/', include('users.urls'), name='users'),
 ]
