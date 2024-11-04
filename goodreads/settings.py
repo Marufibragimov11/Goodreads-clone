7c4e73c055050049f08cb9e2c5ddfe65dc7de1d0
Marufibragimov11 <ibragimovmaruf1108@gmail.com>
2024-11-04 12:57:49 -0500

    added static files and modified

--- goodreads/settings.py
+++ goodreads/settings.py
@@ -100,6 +100,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+MEDIA_ROOT = "media-files"
+MEDIA_URL = "/media/"
+
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- goodreads/urls.py
+++ goodreads/urls.py
@@ -1,5 +1,7 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 from .views import landing_page
 
@@ -9,3 +11,5 @@ urlpatterns = [
     path("books/", include("books.urls")),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
