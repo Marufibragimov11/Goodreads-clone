f9649f7e019b7ba9ae7495a9837fe851fbcac970
Marufibragimov11 <ibragimovmaruf1108@gmail.com>
2024-11-13 05:54:51 -0500

    added pagination

--- books/views.py
+++ books/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import ListView, DetailView
@@ -6,17 +7,24 @@ from books.models import Book
 
 
 # # Generic View
-class BooksView(ListView):
-    template_name = "books/list.html"
-    queryset = Book.objects.all()
-    context_object_name = "books"
+# class BooksView(ListView):
+#     template_name = "books/list.html"
+#     queryset = Book.objects.all()
+#     context_object_name = "books"
+#     paginate_by = 2
 
 
-# # Simple View
-# class BooksView(View):
-#     def get(self, request):
-#         books = Book.objects.all()
-#         return render(request, "books/list.html", {"books": books})
+# Simple View
+class BooksView(View):
+    def get(self, request):
+        books = Book.objects.all().order_by('id')
+        page_size = request.GET.get('page_size', 2)
+        paginator = Paginator(books, page_size)
+
+        page_num = request.GET.get('page', 1)
+        page_obj = paginator.get_page(page_num)
+
+        return render(request, "books/list.html", {"page_obj": page_obj})
 
 
 class BookDetailedView(DetailView):
